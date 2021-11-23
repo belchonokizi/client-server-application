26996adf1924f107b96427d7440aaab99c643dfc
Ilzira <belchonokizi@yandex.ru>
2021-11-23 16:39:15 +0300

    first_commit

---
+++ src/sample/Config.java
@@ -0,0 +1,9 @@
+package sample;
+
+//public class Config {
+//    protected String dbHost = ;
+//    protected String dbPort = ;
+//    protected String dbUser = ;
+//    protected String dbPass = ;
+//    protected String dbName = ;
+//}
---
+++ src/sample/controller/Controller.java
@@ -0,0 +1,44 @@
+package sample.controller;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Button showButton;
+
+    @FXML
+    void initialize() {
+        showButton.setOnAction(event -> {
+            showButton.getScene().getWindow().hide();
+
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/sample/view/app.fxml"));
+            try {
+                loader.load();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            Parent root = loader.getRoot();
+            Stage stage = new Stage();
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+        });
+    }
+}
